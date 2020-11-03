11a2862687c8d36f882927ebe02d97aa79b8463b
ywu1741 <60327122+ywu1741@users.noreply.github.com>
2020-11-03 12:28:38 -0500

    Update lrCostFunction.m

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -12,16 +12,6 @@ m = length(y); % number of training examples
 J = 0;
 grad = zeros(size(theta));
 
-
-hx = sigmoid(X*theta);
-e1 = sum((-y).*log(hx));
-e2 = sum((y-1).*log(1-hx));
-si = size(theta)(1);
-J = (1/m)*(e1+e2)+(lambda/(2*m))*sum(theta(2:si).*theta(2:si));
-
-grad = (1/m)*X'*(hx-y)+(lambda/m)*theta;
-grad(1) = grad(1)-(lambda/m)*theta(1);
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
 %               You should set J to the cost.
@@ -37,20 +27,16 @@ grad(1) = grad(1)-(lambda/m)*theta(1);
 %       prediction for that example. You can make use of this to vectorize
 %       the cost function and gradient computations. 
 %
-% Hint: When computing the gradient of the regularized cost function, 
-%       there're many possible vectorized solutions, but one solution
-%       looks like:
-%           grad = (unregularized gradient for logistic regression)
-%           temp = theta; 
-%           temp(1) = 0;   % because we don't add anything for j = 0  
-%           grad = grad + YOUR_CODE_HERE (using the temp variable)
-%
 
 
+hx = sigmoid(X*theta);
+e1 = sum((-y).*log(hx));
+e2 = sum((y-1).*log(1-hx));
+si = size(theta)(1);
+J = (1/m)*(e1+e2)+(lambda/(2*m))*sum(theta(2:si).*theta(2:si));
 
-
-
-
+grad = (1/m)*X'*(hx-y)+(lambda/m)*theta;
+grad(1) = grad(1)-(lambda/m)*theta(1);
 
 
 
